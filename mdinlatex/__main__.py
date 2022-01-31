5c49c4a83148794f0bbe5339564f901eeef541dc
franceme <9311496+franceme@users.noreply.github.com>
2022-01-31 15:07:41 -0500

    Update

--- mdinlatex/__main__.py
+++ mdinlatex/__main__.py
@@ -53,7 +53,7 @@ def grab_code(line):
     \\begin{{lstlisting}}[language=Python,escapechar=@,caption={{CAPTION HERE}}, captionpos=b,label={{lst:python_label}}]
 {result.text.strip()} 
     \\end{{lstlisting}}
-\\end{figure}
+\\end{{figure}}
 """
 
     return line
