94689a67f4c9f376f439237f0ada7350a8d5d30d
franceme <9311496+franceme@users.noreply.github.com>
2022-01-31 15:04:06 -0500

    Update

--- mdinlatex/__main__.py
+++ mdinlatex/__main__.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 					section_name = None
 					prefix = None
 				elif line.contains("<code>"):
-					rawd = grab_code(no_tags(line,"p"))
+					rawd = grab_code(line)
 					section_name = None
 					prefix = None
 				else:
