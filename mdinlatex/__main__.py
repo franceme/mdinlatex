21b1d897cabb0ff3a662c7093fc5185fae49c80d
franceme <9311496+franceme@users.noreply.github.com>
2022-01-30 21:40:12 -0500

    Update

--- mdinlatex/__main__.py
+++ mdinlatex/__main__.py
@@ -37,7 +37,7 @@ def arguments(string_set):
 def grab_link_text(line):
     grab_links = lambda tag: (getattr(tag, 'name', None) == 'a' and 'href' in tag.attrs)
     
-    for result in soup.find_all(grab_links):
+    for result in BeautifulSoup(line).find_all(grab_links):
        newString = f"\\href{{ {result['href']} }} {{ {result.text.strip()} }}" 
        line = line.replace(result, newString)
 
